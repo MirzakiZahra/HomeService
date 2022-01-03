1d9defedd4b1a2b7c6b96d7d9dec87cb0294891e
Zahra <mirzakizahra@gmail.com>
2022-01-03 13:06:36 -0800

    add UserStatuue  enum class

--- src/main/java/entity/HomeServices.java
+++ src/main/java/entity/HomeServices.java
@@ -1,4 +1,18 @@
 package entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class HomeServices {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    List<Expert>experts=new ArrayList<>();
+
 }
