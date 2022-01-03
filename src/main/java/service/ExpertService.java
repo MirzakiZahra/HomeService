954a7fcf476412ebdec15206c45a178544fd2a60
Zahra <mirzakizahra@gmail.com>
2022-01-03 14:29:13 -0800

    add addExpert method in ExpertService class

--- src/main/java/service/ExpertService.java
+++ src/main/java/service/ExpertService.java
@@ -1,4 +1,17 @@
 package service;
 
+import entity.Expert;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 public class ExpertService {
+    static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+    public void addUser(Expert expert){
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(expert);
+        transaction.commit();
+        session.close();
+    }
 }
