97315cfafe3b46367f57ef31921bbe70307c3e3d
Zahra <mirzakizahra@gmail.com>
2022-01-05 16:20:48 -0800

    add findserviceByName in sericeDb

---
+++ src/main/java/dao/ServiceDb.java
@@ -0,0 +1,24 @@
+package dao;
+
+import model.people.Expert;
+import model.services.HomeServices;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class ServiceDb {
+    static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+    public HomeServices findServiceByName(String name) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        String sql = "select * from HomeService where name = :name ";
+        SQLQuery query = session.createSQLQuery(sql);
+        query.addEntity(HomeServices.class);
+        query.setParameter("name", name);
+        HomeServices homeServices= (HomeServices) query.list().get(0);
+        session.close();
+        return homeServices;
+    }
+}
