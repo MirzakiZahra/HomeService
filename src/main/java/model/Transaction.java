3caf62064b804d333e91d29582b93e2acdd43278
Zahra <mirzakizahra@gmail.com>
2022-01-10 19:02:13 -0800

    add date and fee to Transaction Class

--- src/main/java/model/Transaction.java
+++ src/main/java/model/Transaction.java
@@ -1,5 +1,22 @@
 package model;
 
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @CreationTimestamp
+    private Date date;
+    private float fee;
     
 }
