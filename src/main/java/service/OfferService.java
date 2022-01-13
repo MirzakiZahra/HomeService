823ada10ce41d4ac2472bde8a9b782259dcd016c
Zahra <mirzakizahra@gmail.com>
2022-01-12 16:32:32 -0800

    Add  createOffer method in OfferService classes

--- src/main/java/service/OfferService.java
+++ src/main/java/service/OfferService.java
@@ -1,6 +1,7 @@
 package service;
 
 import dao.ExpertDb;
+import dao.OfferDb;
 import model.Offer;
 import model.Order;
 import model.user.Expert;
@@ -9,11 +10,12 @@ import java.util.Date;
 
 public class OfferService {
     ExpertDb expertDb = new ExpertDb();
+    OfferDb offerDb= new OfferDb();
     public void createOffer(float price, Expert expert, Order order,
                             Date creationDate, Date startDate, String email){
         expert = expertDb.findExpertByEmail(email);
-        Offer offer =new Offer(price expert,order,creationDate,startDate);
-
+        Offer offer =new Offer(price, expert,order,creationDate,startDate);
+offerDb.addOffer(offer);
 
     }
 }
