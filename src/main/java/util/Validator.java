78abbde6da5122488884f469a912ff4af3fc7656
Zahra <mirzakizahra@gmail.com>
2022-01-14 11:38:17 -0800

    Add Base Menu to Main Class

--- src/main/java/util/Validator.java
+++ src/main/java/util/Validator.java
@@ -1,5 +1,5 @@
 package util;
 
 public class Validator {
-    
+    public static void checkFirstInput()
 }
--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -1,13 +1,28 @@
 package view;
 
-public class Main  {
-    public static void main(String[] args) {
-        System.out.println("************welcome********");
-        int firstInput =0;
-        do{
-            System.out.println("1.admin\n2.customer\n3.expert\n4.Exit");
+import java.util.Scanner;
 
-        }while (firstInput!=4);
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("************welcome********");
+        String firstInput = new String();
+           do {
+               System.out.println("1.admin\n2.customer\n3.expert\n4.Exit");
+               firstInput = scanner.next();
+               switch (firstInput){
+                   case "1":
+                       //todo
+                   case "2":
+                       //todo
+                   case "3":
+                       //todo
+                   case "4":
+                       break;
+                   default:
+                       System.out.println("Please Enter right Number");
+               }
+           } while (!"4".equals(firstInput));
 
 
 
