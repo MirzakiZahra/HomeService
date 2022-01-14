91fae5e5e468907ca736f9c8082a95c52da2f240
Zahra <mirzakizahra@gmail.com>
2022-01-14 10:51:50 -0800

    Debug createOffer Method in OfferService Class

--- src/main/java/service/OfferService.java
+++ src/main/java/service/OfferService.java
@@ -2,6 +2,7 @@ package service;
 
 import dao.ExpertDb;
 import dao.OfferDb;
+import dao.OrderDb;
 import model.Offer;
 import model.Order;
 import model.user.Expert;
@@ -11,12 +12,13 @@ import java.util.Date;
 public class OfferService {
     ExpertDb expertDb = new ExpertDb();
     OfferDb offerDb = new OfferDb();
+    OrderDb orderDb = new OrderDb();
 
-    public void createOffer(float price, Order order,
+    public void createOffer(float price, int orderId,
                             Date creationDate, Date startDate, String email) {
         Expert expert = expertDb.findExpertByEmail(email);
+        Order order = orderDb.findOrderById(orderId);
         Offer offer = new Offer(price, expert, order, creationDate, startDate);
         offerDb.addOffer(offer);
-
     }
 }
