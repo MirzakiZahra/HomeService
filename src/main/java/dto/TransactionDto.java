acc6141783d3c5ae0476a693ce0f1c9c76d6f8a2
Zahra <mirzakizahra@gmail.com>
2022-01-15 15:29:57 -0800

    Add  Property to TransactionDto class

--- src/main/java/dto/TransactionDto.java
+++ src/main/java/dto/TransactionDto.java
@@ -1,4 +1,23 @@
 package dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import model.Order;
+import model.enums.TypeOfTransaction;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TransactionDto {
+    private int id;
+    private Date date;
+    private TypeOfTransaction typeOfTransaction;
+    private Order order;
 }
