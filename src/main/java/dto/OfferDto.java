a105d091b181cf35e064784c6effd9f6b5bd7d44
Zahra <mirzakizahra@gmail.com>
2022-01-15 15:27:00 -0800

    Add  Property to OfferDto class

--- src/main/java/dto/OfferDto.java
+++ src/main/java/dto/OfferDto.java
@@ -1,4 +1,19 @@
 package dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class OfferDto {
+    private int id;
+    private float price;
+    private Date creationDate;
+    private Date startDate;
 }
