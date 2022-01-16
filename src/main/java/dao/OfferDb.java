77334600edeadb9f2de2417e90f72c14e93f6ca7
Zahra <mirzakizahra@gmail.com>
2022-01-15 20:36:47 -0800

    Change showOffer method by hql

--- src/main/java/dao/OfferDb.java
+++ src/main/java/dao/OfferDb.java
@@ -1,12 +1,14 @@
 package dao;
 
 import model.Offer;
+import model.services.SubService;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import javax.persistence.Query;
 import java.util.List;
 
 public class OfferDb {
@@ -23,10 +25,11 @@ public class OfferDb {
     public List<Offer> showOffer() {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
-        String sql = "select * from offer ";
-        SQLQuery query = session.createSQLQuery(sql);
-        query.addEntity(Offer.class);
-        List<Offer> offerList = query.list();
+        String hql = "from Offer ";
+        Query query = session.createQuery(hql);
+        List<Offer> offerList = query.getResultList();
+        transaction.commit();
+        session.close();
         return offerList;
     }
 }
