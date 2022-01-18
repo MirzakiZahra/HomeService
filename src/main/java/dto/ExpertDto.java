7e48aed50a3a0abd1f7160ab191dbbfb4b186c34
Zahra <mirzakizahra@gmail.com>
2022-01-17 21:04:18 -0800

    Add properties to ExpertDto class

--- src/main/java/dto/ExpertDto.java
+++ src/main/java/dto/ExpertDto.java
@@ -1,2 +1,31 @@
-package dto;public class ExpertDto {
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import model.Offer;
+import model.services.SubService;
+
+import javax.persistence.Column;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ExpertDto {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String username;
+    private float score;
+    private List<SubService> subServiceList = new ArrayList<>();
+    private List<Offer> offerList = new ArrayList<>();
+    private float creditExpert;
+    private int countOfOrder = 0;
 }
