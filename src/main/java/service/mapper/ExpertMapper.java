34b3f70665eb63a8849ebae951d5f32b32a26d7d
Zahra <mirzakizahra@gmail.com>
2022-01-17 21:21:26 -0800

    Add convertExpertToExpertDto Method to ExpertMapper Class

--- src/main/java/service/mapper/ExpertMapper.java
+++ src/main/java/service/mapper/ExpertMapper.java
@@ -1,5 +1,19 @@
 package service.mapper;
 
+import dto.ExpertDto;
+import model.user.Expert;
+
 public class ExpertMapper {
-    
+    public ExpertDto convertExpertToExpertDto(Expert expert){
+        ExpertDto expertDto = ExpertDto.builder()
+                .id(expert.getId())
+                .username(expert.getUsername())
+                .firstName(expert.getFirstName())
+                .lastName(expert.getLastName())
+                .subServiceList(expert.getSubServiceList())
+                .score(expert.getScore())
+                .offerList(expert.getOfferList())
+                .build();
+        return expertDto;
+    }
 }
