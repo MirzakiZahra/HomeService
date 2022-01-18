a6185003619a310e4c76c62a4e8b13281a195491
Zahra <mirzakizahra@gmail.com>
2022-01-17 17:36:54 -0800

    Add AddAddress Method to AddressDb Class

--- src/main/java/dao/AddressDb.java
+++ src/main/java/dao/AddressDb.java
@@ -1,5 +1,18 @@
 package dao;
 
-public class AddressDb {
+import model.Address;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
+public class AddressDb {
+    static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+    public void AddAddress(Address address){
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(address);
+        transaction.commit();
+        session.close();
+    }
 }
