f4e07dde245e6dd8738f6752b4f12bafc87c1583
Zahra <mirzakizahra@gmail.com>
2022-01-18 21:24:34 -0800

    Add convertCustomerToCustomerDto method in customerMapper class

--- src/main/java/service/mapper/CustomerMapper.java
+++ src/main/java/service/mapper/CustomerMapper.java
@@ -1,4 +1,17 @@
 package service.mapper;
 
+import dto.CustomerDto;
+import model.user.Customer;
+
 public class CustomerMapper {
+    public CustomerDto convertCustomerToCustomerDto(Customer customer) {
+        CustomerDto customerDto = CustomerDto.builder()
+                .id(customer.getId())
+                .firstName(customer.getFirstName())
+                .lastName(customer.getLastName())
+                .address(customer.getAddress())
+                .credit(customer.getCredit())
+                .build();
+        return customerDto;
+    }
 }
