bbb6c53fe12d5be0c37100478db83b7ae53b6765
Zahra <mirzakizahra@gmail.com>
2022-01-18 18:28:15 -0800

    Add deleteMainService method in MainServiceService class

--- src/main/java/service/MainServiceService.java
+++ src/main/java/service/MainServiceService.java
@@ -2,6 +2,7 @@ package service;
 
 import dao.ServiceDb;
 import model.services.MainService;
+import model.user.Expert;
 
 public class MainServiceService {
     ServiceDb serviceDb = new ServiceDb();
@@ -9,6 +10,12 @@ public class MainServiceService {
     public void createMainService(String name) {
         MainService mainService = new MainService(name);
         serviceDb.addMainService(mainService);
+
+    }
+    public void deleteMainService(String name) {
+        MainService mainService=serviceDb.findServiceByName(name);
+        serviceDb.deleteMainService(mainService);
+
 
     }
 }
