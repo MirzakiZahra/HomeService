c63aecd50a6b05abe62e156a6af8ea41b625dfff
Zahra <mirzakizahra@gmail.com>
2022-01-18 20:12:50 -0800

    Add  convertTransactionToTransactionDto Method in TransactionMapper class

--- src/main/java/service/mapper/TransactionMapper.java
+++ src/main/java/service/mapper/TransactionMapper.java
@@ -5,6 +5,9 @@ import dto.TransactionDto;
 import model.Order;
 import model.Transaction;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TransactionMapper {
     public TransactionDto convertTransactionToTransactionDto(Transaction transaction){
         TransactionDto transactionDto=TransactionDto.builder()
@@ -13,5 +16,14 @@ public class TransactionMapper {
                 .typeOfTransaction(transaction.getTypeOfTransaction())
                 .build();
         return transactionDto;
+    }
+    public List<TransactionDto> convertTransactionToTransactionDto(List<Transaction>
+                                                                           transactionList) {
+        List<TransactionDto> transactionDtos = new ArrayList<>();
+        for (Transaction transaction : transactionList) {
+            TransactionDto transactionDto=convertTransactionToTransactionDto(transaction);
+            transactionDtos.add(transactionDto);
+        }
+        return transactionDtos;
     }
 }
