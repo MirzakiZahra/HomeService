dd8edca4999cffc858c7842db5ad3aec69f1fd00
Zahra <mirzakizahra@gmail.com>
2022-01-18 21:14:57 -0800

    Add convertTransactionDtoToTransaction method in transactionMapper class

--- src/main/java/service/mapper/TransactionMapper.java
+++ src/main/java/service/mapper/TransactionMapper.java
@@ -1,29 +1,41 @@
 package service.mapper;
 
-import dto.OrderDto;
 import dto.TransactionDto;
-import model.Order;
 import model.Transaction;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionMapper {
-    public TransactionDto convertTransactionToTransactionDto(Transaction transaction){
-        TransactionDto transactionDto=TransactionDto.builder()
+    public TransactionDto convertTransactionToTransactionDto(Transaction transaction) {
+        TransactionDto transactionDto = TransactionDto.builder()
                 .id(transaction.getId())
                 .date(transaction.getDate())
                 .typeOfTransaction(transaction.getTypeOfTransaction())
                 .build();
         return transactionDto;
     }
+
     public List<TransactionDto> convertTransactionToTransactionDto(List<Transaction>
                                                                            transactionList) {
         List<TransactionDto> transactionDtos = new ArrayList<>();
         for (Transaction transaction : transactionList) {
-            TransactionDto transactionDto=convertTransactionToTransactionDto(transaction);
+            TransactionDto transactionDto = convertTransactionToTransactionDto(transaction);
             transactionDtos.add(transactionDto);
         }
         return transactionDtos;
+    }
+
+    public List<Transaction> convertTransactionDtoToTransaction(List<TransactionDto>
+                                                                        transactionDtoList) {
+        List<Transaction> transactionList = new ArrayList<>();
+        for (TransactionDto transactionDto : transactionDtoList) {
+            Transaction transaction = Transaction.builder()
+                    .date(transactionDto.getDate())
+                    .typeOfTransaction(transactionDto.getTypeOfTransaction())
+                    .build();
+            transactionList.add(transaction);
+        }
+        return transactionList;
     }
 }
