e7fc3c89c6cee336e010b1be7d478e194cf6c89f
Zahra <mirzakizahra@gmail.com>
2022-01-23 23:58:40 -0800

    Add convertMainServiceToMainServiceDto Method to MAinServiceMapper Class

--- src/main/java/service/mapper/MainServiceMapper.java
+++ src/main/java/service/mapper/MainServiceMapper.java
@@ -1,4 +1,14 @@
 package service.mapper;
 
+import dto.MainServiceDto;
+import model.services.MainService;
+
 public class MainServiceMapper {
+    public MainServiceDto convertMainServiceToMainServiceDto(MainService mainService){
+        MainServiceDto mainServiceDto = MainServiceDto.builder()
+                .name(mainService.getName())
+                .subServiceSet(mainService.getSubServiceSet())
+                .build();
+        return mainServiceDto;
+    }
 }
