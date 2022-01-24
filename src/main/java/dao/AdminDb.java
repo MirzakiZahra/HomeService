81bfc4010db735b8b1920192cb6a8f8013a2b7eb
Zahra <mirzakizahra@gmail.com>
2022-01-24 15:25:41 -0800

    Add  addAdmin Method toadminDb  class

--- src/main/java/dao/AdminDb.java
+++ src/main/java/dao/AdminDb.java
@@ -1,4 +1,20 @@
 package dao;
 
+import model.user.Expert;
+import model.user.Manager;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class AdminDb {
+    static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+    public void addAdmin(Manager manager) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(manager);
+        transaction.commit();
+        session.close();
+    }
 }
