bb7df812406c8c52d0811d64e24f4dbfaddbb494
Zahra <mirzakizahra@gmail.com>
2022-01-24 12:37:19 -0800

    Add convertSubServiceToSubServiceDto Method to subServiceMaper class

--- src/main/java/service/mapper/SubServiceMapper.java
+++ src/main/java/service/mapper/SubServiceMapper.java
@@ -1,4 +1,17 @@
 package service.mapper;
 
+import dto.MainServiceDto;
+import dto.SubServiceDto;
+import model.services.MainService;
+import model.services.SubService;
+
 public class SubServiceMapper {
+    public SubServiceDto convertSubServiceToSubServiceDto(SubService subService){
+        SubServiceDto subServiceDto = SubServiceDto.builder()
+                .name(subService.getName())
+                .price(subService.getPrice())
+                .description(subService.getDescription())
+                .build();
+        return subServiceDto;
+    }
 }
