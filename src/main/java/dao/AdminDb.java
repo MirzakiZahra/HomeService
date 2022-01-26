9fd78a6a133c9c3cd1bbd4aab51db0a5387e2b96
Zahra <mirzakizahra@gmail.com>
2022-01-25 17:01:01 -0800

    Add  findManagerByEmail method to AdminDb  class

--- src/main/java/dao/AdminDb.java
+++ src/main/java/dao/AdminDb.java
@@ -1,11 +1,15 @@
 package dao;
 
+import model.user.Customer;
 import model.user.Expert;
 import model.user.Manager;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import javax.persistence.Query;
+import java.util.List;
 
 public class AdminDb {
     static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
@@ -16,5 +20,16 @@ public class AdminDb {
         session.save(manager);
         transaction.commit();
         session.close();
+    }
+    public List<Manager> findManagerByEmail(String email) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        String hql = "from Manager m where m.email = :email";
+        Query query = session.createQuery(hql);
+        query.setParameter("email", email);
+        List<Manager> managerList = query.getResultList();
+        transaction.commit();
+        session.close();
+        return managerList;
     }
 }
