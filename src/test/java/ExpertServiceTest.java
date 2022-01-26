d974d5024ebe6d51f8f76a24c5b268143cd6fe15
Zahra <mirzakizahra@gmail.com>
2022-01-25 21:24:13 -0800

    Add giveExpertEmail_findExpertByEmail_trueExpertName method to ExpertServiceTest Class

--- src/test/java/ExpertServiceTest.java
+++ src/test/java/ExpertServiceTest.java
@@ -1,6 +1,9 @@
 import dto.AddressDto;
+import dto.ExpertDto;
 import model.user.Expert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import service.ExpertService;
 
 public class ExpertServiceTest {
@@ -10,6 +13,10 @@ public class ExpertServiceTest {
     void init() {
         expertService.createExpert("Ali", "Alavi", "ali@gmail.com");
     }
-    
+    @Test
+    void giveExpertEmail_findExpertByEmail_trueExpertName(){
+        ExpertDto expertDto = expertService.findExpertByEmail("ali@gmail.com");
+        Assertions.assertEquals("Ali",expertDto.getFirstName());
+    }
 }
 
