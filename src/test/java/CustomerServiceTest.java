b492b9d8f552fe1872477c50d0f924b8845730b8
Zahra <mirzakizahra@gmail.com>
2022-01-25 20:49:22 -0800

    Rename All Methods to Standard Format in CustomerServiceTest Class

--- src/test/java/CustomerServiceTest.java
+++ src/test/java/CustomerServiceTest.java
@@ -11,10 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerServiceTest {
-    Customer customer = new Customer();
-    Address address = new Address();
     AddressDto addressDto = new AddressDto();
-    List<Address> addressList = new ArrayList<>();
     CustomerService customerService = new CustomerService();
     @BeforeEach
     void init(){
@@ -24,26 +21,27 @@ public class CustomerServiceTest {
                 .street("Imam")
                 .plaque("5")
                 .build();
-        addressList.add(address);
         customerService.createCustomer("Zahra","Mirzaki",
                 addressDto,"mirzaki1@gmail.com","Z@hra123456");
     }
     @Test
-    void saveCustomerThenCheckExistence(){
+    void giveExistCustomerEmail_findByEmail_customerName(){
         Customer customer = customerService.findCustomerByEmail("mirzaki1@gmail.com");
         Assertions.assertEquals(customer.getFirstName(),"Zahra");
     }
     @Test
-    void saveCustomerThenRemoveAndThenCheckExistence(){
+    void giveExistUser_removeAndFindCustomer_ThrowException(){
         customerService.removeCustomer("mirzaki1@gmail.com");
         InputException result = Assertions.assertThrows(InputException.class, () ->
                 customerService.findCustomerByEmail("mirzaki1@gmail.com"));
         Assertions.assertEquals(result.getMessage(),"Customer Not Exist");
     }
     @Test
-    void checkOldPasswordByWrongPass(){
+    void giveWrongPass_checkOldPassword_ThrowException(){
         InputException result = Assertions.assertThrows(InputException.class, () ->
                 customerService.checkOldPassword("123456"));
         Assertions.assertEquals(result.getMessage(),"Password is Incorrect");
     }
+    @Test
+    void
 }
