bde3423cd458bb824eed7e1ba6f5f1e169e4a87b
Zahra <mirzakizahra@gmail.com>
2022-01-27 14:07:43 -0800

    Add addTransaction to TransactionDao Class

--- src/main/java/dao/TransactionDao.java
+++ src/main/java/dao/TransactionDao.java
@@ -1,4 +1,18 @@
 package dao;
 
+import model.Transaction;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class TransactionDao {
+    static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+    public void addTransaction(Transaction transaction){
+        Session session = sessionFactory.openSession();
+        org.hibernate.Transaction transaction1=  session.beginTransaction();
+        session.save(transaction);
+        transaction1.commit();
+        session.close();
+    }
 }
