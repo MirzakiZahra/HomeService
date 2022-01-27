3e89f2ecc4e55b79c3c22fc4ede18a5cd874d4a8
Zahra <mirzakizahra@gmail.com>
2022-01-26 19:22:03 -0800

    Add giveAddressNeededData_addAddressAndThenFindIt_AccurateAddress method to AddressServiceTest Class

--- src/test/java/AddressServiceTest.java
+++ src/test/java/AddressServiceTest.java
@@ -1,3 +1,21 @@
+import dao.AddressDb;
+import model.Address;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import service.AddressService;
+
 public class AddressServiceTest {
-    
+    AddressService addressService = new AddressService();
+    AddressDb addressDb = new AddressDb();
+    @Test
+    void giveAddressNeededData_addAddressAndThenFindIt_AccurateAddress(){
+        Address address = Address.builder()
+                .country("Iran")
+                .city("Tehran")
+                .street("Imam")
+                .plaque("6")
+                .build();
+        addressService.createAddress(address);
+        Assertions.assertEquals(address,addressDb.findAddressById(1));
+    }
 }
