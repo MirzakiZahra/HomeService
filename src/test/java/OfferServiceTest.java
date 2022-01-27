faf69d82a7325f4b15bb50390451d51cfadefb57
Zahra <mirzakizahra@gmail.com>
2022-01-27 15:22:09 -0800

    Add giveRequiredOfferData_createOfferThenFindOffer_createAndFindAccurately Method OfferServiceTest Class

--- src/test/java/OfferServiceTest.java
+++ src/test/java/OfferServiceTest.java
@@ -1,3 +1,53 @@
+import dto.AddressDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class OfferServiceTest {
-    
+    OfferService offerService = new OfferService();
+    ExpertService expertService = new ExpertService();
+    CustomerService customerService = new CustomerService();
+    MainServiceService mainServiceService = new MainServiceService();
+    SubServiceService subServiceService = new SubServiceService();
+    OrderService orderService = new OrderService();
+    @BeforeEach
+    void init(){
+        AddressDto addressDto = AddressDto.builder()
+                .country("Iran")
+                .city("Tehran")
+                .street("Imam")
+                .plaque("5")
+                .build();
+        customerService.createCustomer("Zahra","Mirzaki",
+                addressDto,"mirzaki1@gmail.com","Z@hra123456");
+        mainServiceService.createMainService("Cleaning");
+        subServiceService.createSubService("CleanHome","Nothing",8000,
+                "Cleaning");
+        orderService.createOrder(80000,"Nothing to say",
+                convertStringToDate("12/11/1400,20:00"), convertStringToDate("12/11/1400,21:00"),
+                "Tehran","mirzaki1@gmail.com",1);
+    }
+    @Test
+    void giveRequiredOfferData_createOfferThenFindOffer_createAndFindAccurately(){
+        expertService.createExpert("Ali", "Alavi", "ali@gmail.com");
+        offerService.createOffer(10000,1,8000,convertStringToDate("12/11/1400,20:00"),
+                convertStringToDate("10/11/1400,21:00"),"ali@gmail.com");
+        Assertions.assertEquals(1,offerService.findOfferById(1).getId());
+    }
+    public static Date convertStringToDate(String date) {
+        Date date1 = new Date();
+        SimpleDateFormat formatter =
+                new SimpleDateFormat("dd/MM/yyyy,HH:mm");
+        try {
+            date1 = formatter.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date1;
+    }
 }
