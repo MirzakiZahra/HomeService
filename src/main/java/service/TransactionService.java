4542e59e68aec323e82a2abefd52f07b7fac72fb
Zahra <mirzakizahra@gmail.com>
2022-01-27 14:11:41 -0800

    Add createTransaction to TransactionService Class

--- src/main/java/service/TransactionService.java
+++ src/main/java/service/TransactionService.java
@@ -1,5 +1,18 @@
 package service;
 
-public class TransactionService {
+import dao.TransactionDao;
+import model.Orders;
+import model.Transaction;
+import model.enums.TypeOfTransaction;
 
+public class TransactionService {
+    TransactionDao transactionDao = new TransactionDao();
+    OrderService orderService = new OrderService();
+    public void createTransaction(int orderId, TypeOfTransaction typeOfTransaction){
+        Transaction transaction = new Transaction();
+        Orders orders = orderService.findOrderByIdReturnOrder(orderId);
+        transaction.setOrders(orders);
+        transaction.setTypeOfTransaction(typeOfTransaction);
+        transactionDao.addTransaction(transaction);
+    }
 }
