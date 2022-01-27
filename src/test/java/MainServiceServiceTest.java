b80ef4bc3140044f14713dc64d7c4ac147f5e1ec
Zahra <mirzakizahra@gmail.com>
2022-01-26 19:40:52 -0800

    Add giveMainServiceName_addItAndThenGetIt_AccurateMainService Method to MainServiceService Class

--- src/test/java/MainServiceServiceTest.java
+++ src/test/java/MainServiceServiceTest.java
@@ -1,3 +1,16 @@
+import model.services.MainService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import service.MainServiceService;
+
 public class MainServiceServiceTest {
-    
+    MainServiceService mainServiceService = new MainServiceService();
+
+    @Test
+    void giveMainServiceName_addItAndThenGetIt_AccurateMainService(){
+        MainService mainService = new MainService("Repairing");
+        mainServiceService.createMainService(mainService.getName());
+        Assertions.assertEquals(mainService.getName(),
+                mainServiceService.findMainService("Repairing").get(0).getName());
+    }
 }
