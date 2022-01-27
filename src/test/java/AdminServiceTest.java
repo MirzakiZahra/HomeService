480637721a434a54d79085f7c6984bab32bc6ffa
Zahra <mirzakizahra@gmail.com>
2022-01-26 19:32:17 -0800

    Add AdminServiceTest Class and it's only method

---
+++ src/test/java/AdminServiceTest.java
@@ -0,0 +1,19 @@
+import dao.AdminDb;
+import model.user.Manager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import service.AdminService;
+
+public class AdminServiceTest {
+    AdminService adminService = new AdminService();
+    AdminDb adminDb = new AdminDb();
+    @Test
+    void giveAdminNeededData_addAdminAndThenFindIt_AccurateAdmin(){
+        Manager manager = new Manager("admin","admin","admin@admin.com",
+                "@Dmin123456","admin") ;
+        adminService.createAdmin(manager.getFirstName(), manager.getLastName(), manager.getEmail(),
+                manager.getPassword(), manager.getUsername());
+        Assertions.assertEquals(manager,
+                adminDb.findManagerByEmail("admin@admin.com").get(0));
+    }
+}
