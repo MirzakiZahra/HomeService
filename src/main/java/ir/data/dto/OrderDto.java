09fa770de2bde57bfc934a0e70c0e7de0ca069db
Zahra <mirzakizahra@gmail.com>
2022-01-28 19:38:25 -0800

    Debug OrderDto Class

--- src/main/java/ir/data/dto/OrderDto.java
+++ src/main/java/ir/data/dto/OrderDto.java
@@ -1,20 +1,20 @@
 package ir.data.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import ir.data.model.Offer;
 import ir.data.model.services.SubService;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@Data
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+
 public class OrderDto {
     private int id;
     private float price;
@@ -24,4 +24,16 @@ public class OrderDto {
     private Date endingTime;
     private String address;
     private List<Offer> offerList= new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "OrderDto{" +
+                "id=" + id +
+                ", price=" + price +
+                ", explanation='" + explanation + '\'' +
+                ", beggingDate=" + beggingDate +
+                ", endingTime=" + endingTime +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
