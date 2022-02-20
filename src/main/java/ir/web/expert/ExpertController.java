764a2b76498b43d194ae7235220dd207336f08a7
Zahra <mirzakizahra@gmail.com>
2022-02-20 10:29:55 -0800

    Add displaySignUpPageExpert in   ExpertController class

--- src/main/java/ir/web/expert/ExpertController.java
+++ src/main/java/ir/web/expert/ExpertController.java
@@ -7,5 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequiredArgsConstructor
 public class ExpertController {
-   
+    @RequestMapping(value = "/displaySignUpExpert")
+    public String displaySignUpPageExpert() {
+        return "";
+    }
 }
