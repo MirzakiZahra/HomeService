e8434d4ccb05427eb2626c9b912f785ea1d42166
Zahra <mirzakizahra@gmail.com>
2022-02-20 14:12:29 -0800

    Add displayChangePasswordExpert in ExpertController

--- src/main/java/ir/web/expert/ExpertController.java
+++ src/main/java/ir/web/expert/ExpertController.java
@@ -26,4 +26,8 @@ public class ExpertController {
                 expertDto.getEmail());
         return "expert/expertMainPage";
     }
+    @RequestMapping(value = "/displayChangeExpert")
+    public String displayChangePasswordExpert() {
+        return "";
+    }
 }
