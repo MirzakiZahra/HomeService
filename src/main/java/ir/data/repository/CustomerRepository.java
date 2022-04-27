a30ce1ea64879146e0aa81b7db7a9e5897483421
Zahra <mirzakizahra@gmail.com>
2022-04-27 16:16:21 -0700

    Add findByEmailAndPassword method to CustomerRepository Class

--- src/main/java/ir/data/repository/CustomerRepository.java
+++ src/main/java/ir/data/repository/CustomerRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
     List<Customer> findAllByPassword(String password);
 
     List<Customer> findAllByEmail(String email);
+
+    Optional<Customer> findByEmailAndPassword(String email, String password);
 }
